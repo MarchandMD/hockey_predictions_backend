eaa256d3d20505ab38470676c26a795b2cdfbf96
Michael Marchand <michaeldavidmarchand@gmail.com>
2023-04-21 09:29:00 -0600

    exposes data via endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      get '/todays_games', to: 'hockey_games#index'
+    end
+  end
 end
