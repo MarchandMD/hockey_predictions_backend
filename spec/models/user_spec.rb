fb35edc6491af4df711a9493107d28b595f803cd
Michael Marchand <michaeldavidmarchand@gmail.com>
2023-05-01 16:22:52 -0600

    Begins testing the user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    user = create(:user)
+  end
+
+  describe 'relationships' do
+    it { should have_many :predictions }
+  end
 end
