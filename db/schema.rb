908fe3668437066ac73ba1a00ae25858c28a7aa0
Michael Marchand <michaeldavidmarchand@gmail.com>
2023-05-08 15:34:27 -0600

    adds total_points to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_01_204102) do
+ActiveRecord::Schema.define(version: 2023_05_08_213326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2023_05_01_204102) do
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "total_points", default: 0
   end
 
   add_foreign_key "predictions", "users"
