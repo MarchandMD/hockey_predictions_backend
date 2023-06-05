fe89e0a6fb793f8f8e28277ccc4332814aca3094
Michael Marchand <michaeldavidmarchand@gmail.com>
2023-06-04 19:41:59 -0600

    updates hockeygames#index

--- app/controllers/api/v1/hockey_games_controller.rb
+++ app/controllers/api/v1/hockey_games_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::HockeyGamesController < ApplicationController
   def index
-    render json: HockeyGameSerializer.new(HockeyDataFacade.new.todays_games(date: params[:date]))
+    if params[:date].nil?
+      render json: HockeyGameSerializer.new(HockeyDataFacade.new.todays_games(date: '2023-06-03'))
+    else
+      render json: HockeyGameSerializer.new(HockeyDataFacade.new.todays_games(date: params[:date]))
+    end
   end
 
   def show
